517fa295889f192c87f6b3328f6b80fd3fcc96d2
陈丹 <dan.chen@xiaoyanggroup.com>
2024-12-04 15:59:43 +0800

    提交

--- README.md
+++
@@ -1,2 +0,0 @@
-# python-demo
-poetry 样式
--- demo.py
+++
@@ -1,8 +0,0 @@
-# demo.py
-
-def greet(name: str) -> str:
-    """返回问候语"""
-    return f"Hello, {name}!"
-
-if __name__ == "__main__":
-    print(greet("World"))
